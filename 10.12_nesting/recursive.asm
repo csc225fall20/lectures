a9285522427ce95a4fc3140b7f197848848ff4eb
Christopher Siu <christopher@siu.sh>
2020-10-12 11:54:11 -0700

    Example of infinitely recursive subroutine.

--- 10.12_nesting/recursive.asm
+++ 10.12_nesting/recursive.asm
@@ -6,7 +6,7 @@ FOO     ADD R0, R0, #-1 ; Decrement R0.
         BRnz DONE       ; If R0 is positive...
         ST  R7, SAVER7  ; ...recurse...
         JSR FOO
-        LD  R7, SAVER7
-DONE    RET
+        LD  R7, SAVER7  ; For all R0 >= 3, this "returns to itself", as SAVER7
+DONE    RET             ;  has been overwritten by subsequent recursive calls.
 SAVER7  .FILL x0000
         .END
